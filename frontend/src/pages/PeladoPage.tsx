1c93d3df8273854be8c4de54d3ce64320a818b5c
STB <natalyferrufino43@gmail.com>
2026-08-14 16:29:31 -0600

    Correcciones y avances

--- frontend/src/pages/PeladoPage.tsx
+++ frontend/src/pages/PeladoPage.tsx
@@ -74,8 +74,8 @@ export default function PeladoPage() {
       </Paper>
 
       <PeladoLibrasHoyCards />
-      <PeladoPorSalaTable />
       <PeladoEstiloWidget />
+      <PeladoPorSalaTable />
     </Stack>
   );
 }
